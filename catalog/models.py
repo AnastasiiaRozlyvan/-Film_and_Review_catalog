fdae9aaf61ab3d5cc97e5e7d01d4f3204b5dc4b1
AnastasiiaRozlyvan <nastia.2.r.2@gmail.com>
2019-04-05 19:29:08 +0300

    first try

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Bd(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField(null=True, blank=True)
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.http import HttpResponse
+from .models import Bd
+def index(request) :
+    s = "Here will be film and review catalog.\r\n"
+    for bb in Bd.objects.order_by('-title'):
+        s += bb.title  + bb.content
+    return HttpResponse(s)
