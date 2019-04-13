1a0cf9d08375ba4d15bd8fb90993e6986cc674ed
AnastasiiaRozlyvan <nastia.2.r.2@gmail.com>
2019-04-13 18:19:34 +0300

    new tables

--- catalog/admin.py
+++ catalog/admin.py
@@ -2,12 +2,13 @@ from django.contrib import admin
 from .models import Movie
 from .models import MovieDescription
 from .models import Comments
+from .models import Cast, Staff
 
 
 class MovieAdmin(admin.ModelAdmin):
-    list_display = ('title', 'content', 'year')
-    list_display_links = ('title', 'content', 'year')
-    search_fields = ('title', 'content', 'year')
+    list_display = ('title', 'directed_by', 'year', 'genre')
+    list_display_links = ('title', 'directed_by', 'year', 'genre')
+    search_fields = ('title', 'directed_by', 'year', 'genre')
 
 
 class MovieDescriptionAdmin(admin.ModelAdmin):
@@ -22,6 +23,20 @@ class CommentsAdmin(admin.ModelAdmin):
     search_fields = ('text', 'date_stamp')
 
 
+class CastAdmin(admin.ModelAdmin):
+    list_display = ('name', 'role')
+    list_display_links = ('name', 'role')
+    search_fields = ('name', 'role')
+
+
+class StaffAdmin(admin.ModelAdmin):
+    list_display = ('name', 'position')
+    list_display_links = ('name', 'position')
+    search_fields = ('name', 'position')
+
+
 admin.site.register(Movie, MovieAdmin)
 admin.site.register(MovieDescription, MovieDescriptionAdmin)
 admin.site.register(Comments, CommentsAdmin)
+admin.site.register(Cast, CastAdmin)
+admin.site.register(Staff, StaffAdmin)
