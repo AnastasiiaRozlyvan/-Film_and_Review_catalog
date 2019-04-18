9904494b5197a14e36a2bc81a603c0a3e4a69b8a
AnastasiiaRozlyvan <nastia.2.r.2@gmail.com>
2019-04-18 19:59:14 +0300

    Comments added.

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from .models import Movie
 from .models import MovieDescription
-from .models import Comments
 from .models import Cast, Staff
 
 
@@ -15,12 +14,6 @@ class MovieDescriptionAdmin(admin.ModelAdmin):
     list_display = ('synopsis', 'rate')
     list_display_links = ('synopsis', 'rate')
     search_fields = ('synopsis', 'rate')
-
-
-class CommentsAdmin(admin.ModelAdmin):
-    list_display = ('text', 'date_stamp')
-    list_display_links = ('text', 'date_stamp')
-    search_fields = ('text', 'date_stamp')
 
 
 class CastAdmin(admin.ModelAdmin):
@@ -37,6 +30,5 @@ class StaffAdmin(admin.ModelAdmin):
 
 admin.site.register(Movie, MovieAdmin)
 admin.site.register(MovieDescription, MovieDescriptionAdmin)
-admin.site.register(Comments, CommentsAdmin)
 admin.site.register(Cast, CastAdmin)
 admin.site.register(Staff, StaffAdmin)
--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import MovieCreateView, CommentCreateView
+from .views import MovieCreateView
 from .views import index, statistics, InfoView
 urlpatterns = [path('', index, name='index'),
                path('add/', MovieCreateView.as_view(), name='add'),
-               path('comment/', CommentCreateView.as_view(), name='comment'),
                path('statistics/', statistics, name='statistics'),
                path('movie/<int:pk>', InfoView.as_view(), name='movie_info'),
 ]
